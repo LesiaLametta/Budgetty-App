3c7bbaccb3cbaae49807fa91eb71d228262b71bb
Lesia Lametta <lesialametta@gmail.com>
2018-11-25 09:10:04 +0300

    Updating the Percentages: UI Controller code clean up

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var budgetController = (function() {
     };
 
 
-Expense.prototype.getPercentage = function() {
-    return this.percentage;
-};
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    };
 
 
 
@@ -141,7 +141,7 @@ Expense.prototype.getPercentage = function() {
             });
         },
 
-        getPercentages: function () {
+        getPercentages: function() {
             var allPerc = data.allItems.exp.map(function(cur) {
 
                 return cur.getPercentage();
@@ -186,7 +186,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     };
 
@@ -258,6 +259,32 @@ var UIController = (function() {
             }
         },
 
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+
+            };
+
+
+
+            nodeListForEach(fields, function(current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+
+                } else {
+                    current.textContent = '---';
+                }
+            });
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -297,7 +324,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 2. Read percentages from the budget controller
         var percentages = budgetCtrl.getPercentages();
 
-        // 3. Update the UI with the new percentages     
+        // 3. Update the UI with the new percentages 
+        UICtrl.displayPercentages(percentages);   
     };
 
 
